902d19b93939a1417f27b00d1fd58b342c06441a
Michael Elkerton <michael@elkerton.ca>
2022-12-31 10:56:49 -0600

    Auto Commit 1056

--- lib/server.dart
+++ lib/server.dart
@@ -1,3 +1,3 @@
 class Server {
-    
+  int get port => 999;
 }
--- test/server_test.dart
+++ test/server_test.dart
@@ -1,1 +1,9 @@
-main() {}
+import 'package:test/test.dart';
+import 'package:xperi_dart_mock/xperi_dart_mock.dart';
+
+main() {
+  test('simple', () {
+    final server = Server();
+    expect(server.port, equals(999));
+  });
+}
