01f77ea1148bfab83a2f32cb0610467041e1a070
Michael Elkerton <michael@elkerton.ca>
2022-12-30 21:26:13 -0600

    init

---
+++ bin/xperi_dart_mock.dart
@@ -0,0 +1,5 @@
+import 'package:xperi_dart_mock/xperi_dart_mock.dart' as xperi_dart_mock;
+
+void main(List<String> arguments) {
+  print('Hello world: ${xperi_dart_mock.calculate()}!');
+}
---
+++ test/xperi_dart_mock_test.dart
@@ -0,0 +1,8 @@
+import 'package:xperi_dart_mock/xperi_dart_mock.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
