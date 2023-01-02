43d0e770bd823c619bcec7e63046a1804268b646
Michael Elkerton <michael@elkerton.ca>
2023-01-02 13:18:45 +0000

    test throwsa

--- lib/server.dart
+++ lib/server.dart
@@ -1,6 +1,31 @@
+import 'dart:io';
+
+import 'package:xperi_dart_mock/error.dart';
+
 /// Handle mock requests
 ///
 ///
 class Server {
-  int get port => 0;
+  HttpServer? httpServer;
+
+  /// Default constructor
+  Server();
+
+  Future<void> bind() async {
+    httpServer = await HttpServer.bind("localhost", 0);
+
+    return;
+  }
+
+  void listen() async {
+    if (httpServer == null) {
+      throw ErrorServerNotBound();
+    }
+
+    await httpServer!.forEach((request) {
+      request.response.close();
+    });
+  }
+
+  int get port => httpServer != null ? httpServer!.port : 0;
 }
--- test/server_test.dart
+++ test/server_test.dart
@@ -1,10 +1,17 @@
 import 'package:test/test.dart';
+import 'package:xperi_dart_mock/error.dart';
 import 'package:xperi_dart_mock/xperi_dart_mock.dart';
 
 main() {
-  test('simple', () {
+  /// test server responds
+  test('DefaultServer', () async {
     final server = Server();
 
+    // calling listen before bind should throw an exception
+    expect(server.listen, throwsA(isA<ErrorServerNotBound>()));
+
+    //await server.bind();
+    // get server uri
     // non-zero port
     expect(server.port, equals(0));
 
